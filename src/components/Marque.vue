09ddddfdc21c6854f4ca09333e145c767c1615cf
Mavlon4658 <72940597+Mavlon4658@users.noreply.github.com>
2024-04-04 01:13:20 +0500

    fixed all bugs

--- src/components/Marque.vue
+++ src/components/Marque.vue
@@ -1,129 +1,139 @@
 <template>
   <div>
-    <div class="animation__img_wrap">
-      <div class="animation_img">
+    <swiper
+      :spaceBetween="35"
+      :slides-per-view="'auto'"
+      :speed="2500"
+      :allow-touch-move="false"
+      :autoplay="{
+        delay: 0,
+        disableOnInteraction: false,
+        reverseDirection: swp
+      }"
+      :modules="modules"
+      @slideChangeTransitionEnd="e => handleSlideChange(e)"
+    >
+      <swiper-slide>
         <img src="@/assets/heads/head1.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head2.svg" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head3.svg" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head4.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head5.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head7.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head8.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head9.svg" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head10.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head11.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head12.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head13.svg" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head14.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head15.svg" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head16.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
         <img src="@/assets/heads/head17.png" alt="" />
+      </swiper-slide>
+    </swiper>
+    <div class="animation__img_wrap" ref="animationImgWrap">
+      <div class="animation_img" ref="animationImg">
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  mounted() {
-    let position2 = $(".animation__img_wrap")[0].getBoundingClientRect();
-    let position = $(".animation_img")[0].getBoundingClientRect();
-    let t = false
-    setInterval(() => {
-      let diffirent = 0
-      if(t) {
+import { Swiper, SwiperSlide } from 'swiper/vue';
 
-      }else {
-        diffirent = position2.width-position.width
-        $(".animation_img").css({
-          transform: `translateX(-${diffirent}px)`
-        })
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Autoplay, Navigation],
+    };
+  },
+  data () {
+    return {
+      swp: false,
+    }
+  },
+  methods: {
+    handleSlideChange(swiper) {
+      if (swiper.isBeginning) {
+        swiper.autoplay.stop();
+      } else {
+        swiper.autoplay.start();
       }
-    }, 3000);
-    console.log(position.width);
-    $(".animation_img").animate(
-      {
-        left: "auto",
-        right: 0,
-      },
-      3000
-    );
+      if (!swiper.isEnd) {
+        swiper.slideNext();
+      }
+    },
+  },
+  mounted() {
   },
 };
 </script>
 
-<style scoped>
-.animation__img_wrap {
-  position: relative;
-  border: 1px solid green;
-  width: 100%;
-  height: 130px;
+<style>
+.swiper {
   border-bottom: 3px solid;
-  overflow: hidden;
-}
-.animation_img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 130px;
-  display: flex;
-  align-items: flex-end;
-  transition: 3s linear all;
-}
-/* .animation_img.active {
-  animation: imgActive 3s linear 1;
-} */
-
-/* @keyframes imgActive {
- from {
-  
- }
-} */
-.animation_img img {
-  height: 130px;
-  transform: translateY(6px);
 }
 
-.animation_img img:nth-child(12),
-.animation_img img:last-child {
+.swiper-wrapper {
+  transition-timing-function: linear;
+}
+
+.swiper-slide {
+  width: auto;
+}
+
+.swiper-slide img {
+  height: 130px;
+  transform: translateY(15px);
+  /* margin-bottom: -7px; */
+}
+
+.swiper-slide:nth-child(12) img,
+.swiper-slide:last-child img{
   transform: translateY(10px);
 }
 
-.animation_img img:not(:last-child) {
-  margin-right: 35px;
-}
-
-/* .carusel {
-  width: 100%;
-  border-bottom: 3px solid;
-  overflow: hidden;
-  display: flex;
-  align-items: flex-end;
-}
-.carusel img {
-  height: 130px;
-  transform: translateY(7px);
-}
-
-.carusel img:not(:last-child) {
-  border: 1px solid red;
-} */
-
-@media (max-width: 768px) {
-  .carusel img {
-    height: 100px;
-  }
-}
-@media (max-width: 428px) {
-  .carusel img {
-    height: 80px;
-    margin: 0 10px;
-  }
-}
-@media (max-width: 375px) {
-  .carusel img {
-    height: 70px;
-    margin: 0 0px;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import jQuery from "jquery";
-const $ = jQuery;
-window.$ = $;
 
 
-createApp(App).use($).use(AOS.init()).mount('#app')
+createApp(App).use(AOS.init()).mount('#app')
